202cd4f87821214cd9daae0df5e84cc77cb34ee7
Audrey Fu <audreyfu14@gmail.com>
2017-11-20 09:48:41 -0800

    fix landing

---
+++ scripts/landing.js
@@ -0,0 +1,24 @@
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+  var revealPoint = function(points) {
+    for (i = 0; i < points.length; i++) {
+      points[i].style.opacity = 1;
+    }
+  }
+}
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
