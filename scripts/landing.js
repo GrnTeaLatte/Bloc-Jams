22366518759717b104bb730e24c11c53e911d8fe
Audrey Fu <audreyfu14@gmail.com>
2018-10-11 23:37:29 -0700

    jQuery Landing completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,26 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-  var revealPoint = function(points) {
-    for (i = 0; i < points.length; i++) {
-      points[i].style.opacity = 1;
-    }
+var animatePoints = function() {
+  var revealPoint = function() {
+      // #7
+      $(this).css({
+          opacity: 1,
+          transform: 'scaleX(1) translateY(0)'
+      });
+  };
+  $.each($('.point'), revealPoint);
+};
+
+$(window).load(function() {
+  if ($(window).height() > 950) {
+      animatePoints();
   }
-}
 
-window.onload = function() {
-  if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
-  }
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-  window.addEventListener('scroll', function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
+  $(window).scroll(function(event) {
+             if ($(window).scrollTop() >= scrollDistance) {
+                 animatePoints();
     }
   });
-}
+});
